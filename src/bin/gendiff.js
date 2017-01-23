6224abf551f946c7fec820924c595da4d0b74d10
anton.azgarovich <anton.azgarovich@gmail.com>
2017-01-24 01:01:35 +0300

    moved gendiff to src/bin/gendiff and fixed global installing

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env babel-node
 
-var program = require('commander');
+import program from 'commander';
 
 program
-  .version('0.0.4')
+  .version('0.0.6')
   .arguments('<first_config> <second_config>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
