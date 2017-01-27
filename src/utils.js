3aab11af251d9bee48c33894e6ad387c6ab5c7c0
anton.azgarovich <anton.azgarovich@gmail.com>
2017-01-28 01:21:59 +0300

    added typeOf utils

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,8 @@
 import path from 'path';
 
+const typeOf = obj =>
+  Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
+
 const getFileExtname = filePath => path.extname(filePath).replace(/\./g, '');
 
-export { getFileExtname }; // eslint-disable-line
+export { getFileExtname, typeOf }; // eslint-disable-line
