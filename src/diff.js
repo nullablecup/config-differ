84b15feb820c51b1ecca631cd9d715384ad54a7a
anton.azgarovich <anton.azgarovich@gmail.com>
2017-01-28 21:59:20 +0300

    finished version of recursive programs

--- src/diff.js
+++ src/diff.js
@@ -1,23 +1,25 @@
 import { union, isObject } from 'lodash';
 
-const isNotChanged = (before, after, key) =>
-  before[key] === after[key];
+const isNotChanged = (before, after, key) => before[key] === after[key];
 
-const isChanged = (before, after, key) =>
-  before[key] !== after[key] && !isObject(after[key]);
+const isChanged = (before, after, key) => before[key] !== after[key] && !isObject(after[key]);
 
 const isAdded = (before, key) => (before[key] === undefined);
 
 const isDeleted = (after, key) => (after[key] === undefined);
+
+const setNested = obj =>
+  Object.keys(obj).map(key =>
+    ({ state: 'notChanged', key, value: isObject(obj[key]) ? setNested(obj[key]) : obj[key] }));
 
 export const getAstDiff = (before: Object, after: Object): Object => {
   const unitedKeys = union(Object.keys(before), Object.keys(after));
 
   return unitedKeys.map((key) => {
     if (isAdded(before, key)) {
-      return { state: 'added', key, value: after[key] };
+      return { state: 'added', key, value: isObject(after[key]) ? setNested(after[key]) : after[key] };
     } else if (isDeleted(after, key)) {
-      return { state: 'deleted', key, value: before[key] };
+      return { state: 'deleted', key, value: isObject(before[key]) ? setNested(before[key]) : before[key] };
     } else if (isNotChanged(before, after, key)) {
       return { state: 'notChanged', key, value: after[key] };
     } else if (isChanged(before, after, key)) {
--- src/display.js
+++ src/display.js
@@ -1,18 +1,27 @@
-const astToPlainText = (astDiff) => {
+import { isObject } from 'lodash';
+
+const astToPlainText = (astDiff, space = '') => {
   const mask = {
-    notChanged: (key, value) => `    ${key}: ${value}\n`,
-    changed: (key, value, oldValue) => `  + ${key}: ${value}\n  - ${key}: ${oldValue}\n`,
-    deleted: (key, value) => `  - ${key}: ${value}\n`,
-    added: (key, value) => `  + ${key}: ${value}\n`,
+    notChanged: (key, value) => `${space}    ${key}: ${value}\n`,
+    changed: (key, value, oldValue) => `${space}  + ${key}: ${value}\n${space}  - ${key}: ${oldValue}\n`,
+    deleted: (key, value) => `${space}  - ${key}: ${value}\n`,
+    added: (key, value) => `${space}  + ${key}: ${value}\n`,
+    nested: (key, value) => `${space}    ${key}: ${value}\n`,
   };
 
   const plainTextMiddle = astDiff.reduce((acc, diffState) => {
     const { state, key, value, oldValue } = diffState;
 
+    if (state === 'nested') {
+      return acc + mask[state](key, astToPlainText(value, `${space}    `), oldValue);
+    } else if ((state === 'added' || state === 'deleted') && isObject(value)) {
+      return acc + mask[state](key, astToPlainText(value, `${space}    `), oldValue);
+    }
+
     return acc + mask[state](key, value, oldValue);
   }, '');
 
-  return `{\n${plainTextMiddle}}`;
+  return `{\n${plainTextMiddle}${space}}`;
 };
 
 const display = (astDiff, format) => {
