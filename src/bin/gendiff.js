d17ad80357dd46a5640650c817faa3d33d111ffe
anton.azgarovich <anton.azgarovich@gmail.com>
2017-01-29 16:48:35 +0300

    increased version

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,7 +4,7 @@ import program from 'commander';
 import diff from '..';
 
 program
-  .version('0.1.2')
+  .version('0.3.0')
   .arguments('<first_config> <second_config>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
