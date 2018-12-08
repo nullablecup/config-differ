e7e54c15b0a50ebd27cea799a50358a1f0f24854
anton.azgarovich <anton.azgarovich@gmail.com>
2018-12-08 22:51:07 +0300

    refactoring test

--- __tests__/test.js
+++ __tests__/test.js
@@ -4,31 +4,32 @@ test('test display pretty', () => {
   const pathToBeforeJson = '__tests__/__fixtures__/before.json';
   const pathToAfterJson = '__tests__/__fixtures__/after.json';
   const result = (
-    '{\n' +
-    '    common: {\n' +
-    '        setting1: Value 1\n' +
-    '      - setting2: 200\n' +
-    '        setting3: true\n' +
-    '      - setting6: {\n' +
-    '            key: value\n' +
-    '        }\n' +
-    '      + setting4: blah blah\n' +
-    '      + setting5: {\n' +
-    '            key5: value5\n' +
-    '        }\n' +
-    '    }\n' +
-    '    group1: {\n' +
-    '      + baz: bars\n' +
-    '      - baz: bas\n' +
-    '        foo: bar\n' +
-    '    }\n' +
-    '  - group2: {\n' +
-    '        abc: 12345\n' +
-    '    }\n' +
-    '  + group3: {\n' +
-    '        fee: 100500\n' +
-    '    }\n' +
-    '}'
+    ''
+    + '{\n'
+    + '    common: {\n'
+    + '        setting1: Value 1\n'
+    + '      - setting2: 200\n'
+    + '        setting3: true\n'
+    + '      - setting6: {\n'
+    + '            key: value\n'
+    + '        }\n'
+    + '      + setting4: blah blah\n'
+    + '      + setting5: {\n'
+    + '            key5: value5\n'
+    + '        }\n'
+    + '    }\n'
+    + '    group1: {\n'
+    + '      + baz: bars\n'
+    + '      - baz: bas\n'
+    + '        foo: bar\n'
+    + '    }\n'
+    + '  - group2: {\n'
+    + '        abc: 12345\n'
+    + '    }\n'
+    + '  + group3: {\n'
+    + '        fee: 100500\n'
+    + '    }\n'
+    + '}'
   );
   expect(compare(pathToBeforeJson, pathToAfterJson)).toBe(result);
 });
